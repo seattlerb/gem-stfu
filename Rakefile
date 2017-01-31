06352f5686d1ba414fa464b0b422bc462c50322f
Ryan Davis <ryand@zenspider.com>
2017-01-30 23:48:35 -0800

    ! Forked entirely and added configuration via ~/.gem/stfu.rc.
    [git-p4: depot-paths = "//src/gem-stfu/dev/": change = 11221]

--- Rakefile
+++ Rakefile
@@ -10,8 +10,6 @@ Hoe.spec "gem-stfu" do
   developer "Ryan Davis", "ryand-ruby@zenspider.com"
 
   license "MIT"
-
-  dependency "gem-shut-the-fuck-up", "> 0"
 end
 
 # vim: syntax=ruby
--- lib/gem/stfu.rb
+++ lib/gem/stfu.rb
@@ -1,5 +1,29 @@
+require "rubygems/installer"
+
 module Gem
   class STFU
-    VERSION = "1.0.0"
+    VERSION = "2.0.0"
+
+    def self.stfu_path
+      @stfu_path ||= File.join Gem.user_home, ".gem", "stfu.rc"
+    end
+
+    def self.stfu_list
+      @stfu ||= if File.exist? stfu_path then
+                  File.readlines(stfu_path).map(&:chomp)
+                else
+                  []
+                end
+    end
+
+    def self.stfu? spec, message
+      message == spec.post_install_message && self.stfu_list.include?(spec.name)
+    end
+  end
+end
+
+Gem::Installer.class_eval do
+  def say(message)
+    super unless Gem::STFU.stfu? spec, message
   end
 end
